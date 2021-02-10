f4a73d286e24fb18887e9faeade0c4fe70ff074f
Fabricio Pujol <Fabricio_2112@hotmail.com>
2021-02-09 22:34:10 -0300

    first commit

---
+++ ChallengeThirdWeek/app/src/main/java/com/fabriciopujol/challengethirdweek/models/Place.kt
@@ -0,0 +1,3 @@
+package com.fabriciopujol.challengethirdweek.models
+
+data class Place(val title: String, val descrition: String, val latitude: Double, val longitude: Double)
---
+++ ChallengeThirdWeek/app/src/main/java/com/fabriciopujol/challengethirdweek/models/UserMap.kt
@@ -0,0 +1,3 @@
+package com.fabriciopujol.challengethirdweek.models
+
+data class UserMap(val title: String, val places: List<Place>)
